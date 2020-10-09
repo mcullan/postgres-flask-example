c1b9345d8fd69fadc2f0bff25236fd56d020a4b1
mcullan <mjcullan@gmail.com>
2020-10-08 23:51:41 -0400

    fixed main method

--- app.py
+++ app.py
@@ -35,3 +35,6 @@ def index():
 
     return time + ': ' + str((lost, claimed))
 
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
